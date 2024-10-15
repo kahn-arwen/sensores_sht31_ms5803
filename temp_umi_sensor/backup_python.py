b932539bc6f2cf698fb4cb2795dfdb9c445344d2
Karen Arwen Ferreira <82468660+kahn-arwen@users.noreply.github.com>
2024-10-15 10:46:50 -0300

    Update backup_python.py

--- temp_umi_sensor/backup_python.py
+++ temp_umi_sensor/backup_python.py
@@ -1,105 +1,161 @@
-import time
-import smbus
-import os
+import adafruit_sht31d, board, ms5803py,time, smbus,os, traceback,sys, time
 from datetime import datetime
-import numpy as np
 
-HOUR_READINGS = 720
-DAY_READINGS = 17280
+day_reading = 17280
+hour_reading = 720
+	
+# Min - Max
+tempC_min = float('inf')
+tempC_max = float('-inf')
+humi_min = float('inf')
+humi_max = float('-inf')
 
-def write_header(file):
-    file.write("Data\t\tHora\t\tTemp_atual\tTemp_med\tTemp_min\tTemp_max\tUmi_atual\tUmi_med\t\tUmi_min\t\tUmi_max\n")
+tempC_min_hour = float('inf')
+tempC_max_hour = float('-inf')
+humi_min_hour = float('inf')
+humi_max_hour = float('-inf')
 
-def main():
-    def is_file_empty(file_sec):
-        return os.stat(file_sec).st_size == 0
-    def is_file_empty(file_hour):
-        return os.stat(file_hour).st_size == 0
-    def is_file_empty(file_day):
-        return os.stat(file_day).st_size == 0
-    file_sec = '/home/tecnico/agsolve/temp_umi_sensor/sec_backup.txt'
-    file_hour = '/home/tecnico/agsolve/temp_umi_sensor/hour_backup.txt'
-    file_day = '/home/tecnico/agsolve/temp_umi_sensor/day_backup.txt'
+# Acumuladores
+tempSumC_day = 0;
+humiditySum_day = 0;
+
+tempSumC_hour = 0;
+humiditySum_hour = 0
+
+totalTempSum = 0
+totalHumSum = 0
+
+totalReadingCount = 0	
+readingCount_hour = 0
+readingCount_day = 0
+
+#medias
+average_temp = 0
+average_humi = 0
+average_temp_hour = 0
+average_humi_hour = 0
+average_temp_day = 0
+average_humi_day = 0
+
+aux = 2
+
+sec_backup = "/home/tecnico/agsolve/temp_umi_sensor/sec_backup_py.txt"
+day_backup = "/home/tecnico/agsolve/temp_umi_sensor/hour_backup_py.txt"
+hour_backup = "/home/tecnico/agsolve/temp_umi_sensor/day_backup_py.txt"
+
+writesht = "Data          Hora     Temp_atual   Temp_med   Temp_min   Temp_max   Umi_atual   Umi_med   Umi_min   Umi_max\n"
+
+# Escreve o cabeçalho apenas se o arquivo estiver vazio
+if not os.path.isfile(sec_backup) or os.path.getsize(sec_backup) == 0:
+    with open(sec_backup, "a") as file:
+        file.write(writesht)
+
+if not os.path.isfile(hour_backup) or os.path.getsize(hour_backup) == 0:
+    with open(hour_backup, "a") as file:
+        file.write(writesht)
+
+if not os.path.isfile(day_backup) or os.path.getsize(day_backup) == 0:
+    with open(day_backup, "a") as file:
+        file.write(writesht)
+        
+print(writesht)
+
+while True:
     
     bus = smbus.SMBus(1)
-    address = 0x44
+    time.sleep(1)
 
-    tempC_min, tempC_max = float('inf'), float('-inf')
-    humi_min, humi_max = float('inf'), float('-inf')
-
-    totalTempSum = totalTempHum = totalReadingCount = 0
-    tempReadingsC_hour, humidityReadings_hour = [], []
-    tempReadingsC_day, humidityReadings_day = [], []
-    
-    print ("Data\t\tHora\t\tTemp_atual\tTemp_med\tTemp_min\tTemp_max\tUmi_atual\tUmi_med\t\tUmi_min\t\tUmi_max\n")
-    
-    try: 
-        with open("/home/tecnico/agsolve/temp_umi_sensor/sec_backup.txt", "a") as secBackup, \
-             open("/home/tecnico/agsolve/temp_umi_sensor/hour_backup.txt", "a") as hourBackup, \
-             open("/home/tecnico/agsolve/temp_umi_sensor/day_backup.txt", "a") as dayBackup:
+    i2c = board.I2C()
+    now = datetime.now()
+    date = now.date()
+    current_time = f"{now.hour:02}:{now.minute:02}:{now.second:02}"
+		
+    try:
+        sensor = adafruit_sht31d.SHT31D(i2c)
+        humidity = sensor.relative_humidity
+        temp = sensor.temperature
+					
+        if(temp > tempC_max):
+            tempC_max = temp
+        if (temp < tempC_min):
+            tempC_min = temp
+				
+        if(humidity > humi_max):
+            humi_max = humidity
+        if(humidity < humi_min):
+            humi_min = humidity
+					
+        if(temp > tempC_max_hour):
+            tempC_max_hour = temp
+        if (temp < tempC_min_hour):
+            tempC_min_hour = temp
+						
+        if(humidity > humi_max_hour):
+            humi_max_hour = humidity
+        if(humidity < humi_min_hour):
+            humi_min_hour = humidity
+						
+        #acum average
+        totalTempSum  += temp;
+        tempSumC_hour += temp
+        tempSumC_day += temp
+					
+        totalHumSum   += humidity
+        humiditySum_day += humidity
+        humiditySum_hour += humidity
+					
+        totalReadingCount = totalReadingCount+1
+					
+        if(totalReadingCount > 0): 
+            average_temp = totalTempSum / totalReadingCount  
+            average_humi =totalHumSum / totalReadingCount
+                
+					
+        print(f"{date}   {current_time}   {temp:.2f}°C     {average_temp:.2f}°C     {tempC_min:.2f}°C   {tempC_max:.2f}°C    {humidity:.2f}RH    {average_humi:.2f}RH    {humi_min:.2f}RH   {humi_max:.2f}RH\n")
+					
+        with open(sec_backup, "a") as file_append:
+            file_append.write(f"{date}   {current_time}   {temp:.2f}°C     {average_temp:.2f}°C     {tempC_min:.2f}°C   {tempC_max:.2f}°C    {humidity:.2f}RH    {average_humi:.2f}RH    {humi_min:.2f}RH   {humi_max:.2f}RH\n")
+					
+					
+        if(readingCount_hour < hour_reading):
+            readingCount_hour = readingCount_hour+1
+        if (readingCount_day < day_reading):
+            readingCount_day = readingCount_day+1
+					
+						
+        if(readingCount_hour == hour_reading):
+            average_temp_hour = tempSumC_hour / readingCount_hour
+            average_humi_hour = humiditySum_hour / readingCount_hour
             
-            if is_file_empty(file_sec):
-                write_header(secBackup)
-            if is_file_empty(file_sec):
-                write_header(hourBackup)
-            if is_file_empty(file_sec):
-                write_header(dayBackup) 
-
-            while True:
-                bus.write_i2c_block_data(address, 0x2C, [0x06])
-                time.sleep(1)
-                data = bus.read_i2c_block_data(address, 0x00, 6)
-
-                cTemp = (((data[0] << 8) + data[1]) * 175.0) / 65535.0 - 45.0
-                humidity = (((data[3] << 8) + data[4])) * 100.0 / 65535.0
-
-                # Atualiza min/max
-                tempC_min = min(tempC_min, cTemp)
-                tempC_max = max(tempC_max, cTemp)
-                humi_min = min(humi_min, humidity)
-                humi_max = max(humi_max, humidity)
-
-                totalTempSum += cTemp
-                totalTempHum += humidity
-                totalReadingCount += 1
+            with open(hour_backup, "a") as file_append:
+                file_append.write(f"{date}   {current_time}   {temp:.2f}°C     {average_temp_hour:.2f}°C     {tempC_min:.2f}°C   {tempC_max:.2f}°C    {humidity:.2f}RH    {average_humi_hour:.2f}RH    {humi_min:.2f}RH   {humi_max:.2f}RH\n")
                 
-                average_temp = totalTempSum / totalReadingCount
-                average_humi = totalTempHum / totalReadingCount
-                
-                now = datetime.now()
-                date = now.strftime("%d/%m/%Y")
-                time_str = now.strftime("%H:%M:%S")
-
-                print(f"{date}\t{time_str}\t{cTemp:.2f} °C\t{average_temp:.2f} °C\t{tempC_min:.2f} °C\t{tempC_max:.2f} °C\t{humidity:.2f} RH\t{average_humi:.2f} RH\t{humi_min:.2f} RH\t{humi_max:.2f} RH")
-                
-                secBackup.write(f"{date}\t{time_str}\t{cTemp:.2f} °C\t{average_temp:.2f} °C\t{tempC_min:.2f} °C\t{tempC_max:.2f} °C\t{humidity:.2f} RH\t{average_humi:.2f} RH\t{humi_min:.2f} RH\t{humi_max:.2f} RH\n")
-                secBackup.flush()
-                
-                tempReadingsC_hour.append(cTemp)
-                humidityReadings_hour.append(humidity)
-                tempReadingsC_day.append(cTemp)
-                humidityReadings_day.append(humidity)
-
-                if len(tempReadingsC_hour) == HOUR_READINGS:
-                    average_temp_hour = np.mean(tempReadingsC_hour)
-                    average_humi_hour = np.mean(humidityReadings_hour)
-                    hourBackup.write(f"{date}\t{time_str}\t{cTemp:.2f} °C\t{average_temp_hour:.2f} °C\t{tempC_min:.2f} °C\t{tempC_max:.2f} °C\t{humidity:.2f} RH\t{average_humi_hour:.2f} RH\t{humi_min:.2f} RH\t{humi_max:.2f} RH\n")
-                    hourBackup.flush()
-                    tempReadingsC_hour.clear()
-                    humidityReadings_hour.clear()
-
-                if len(tempReadingsC_day) == DAY_READINGS:
-                    average_temp_day = np.mean(tempReadingsC_day)
-                    average_humi_day = np.mean(humidityReadings_day)
-                    dayBackup.write(f"{date}\t{time_str}\t{cTemp:.2f} °C\t{average_temp_day:.2f} °C\t{tempC_min:.2f} °C\t{tempC_max:.2f} °C\t{humidity:.2f} RH\t{average_humi_day:.2f} RH\t{humi_min:.2f} RH\t{humi_max:.2f} RH\n")
-                    dayBackup.flush()
-                    tempReadingsC_day.clear()
-                    humidityReadings_day.clear()
-
-                time.sleep(5)
-
+            readingCount_hour = 0;
+            humiditySum_hour = 0;
+            tempSumC_hour = 0;
+						
+            tempC_min_hour = float('inf')
+            tempC_max_hour = float('-inf')
+            humi_min_hour = float('inf')
+            humi_max_hour = float('-inf')
+					
+        if(readingCount_day == day_reading):
+            average_temp_day = tempSumC_day/day_reading
+            average_humi_day = humiditySum_day/day_reading
+            
+            with open(day_backup, "a") as file_append:
+                file_append.write(f"{date}   {current_time}   {temp:.2f}°C     {average_temp_day:.2f}°C     {tempC_min:.2f}°C   {tempC_max:.2f}°C    {humidity:.2f}RH    {average_humi_day:.2f}RH    {humi_min:.2f}RH   {humi_max:.2f}RH\n")
+			
     except Exception as e:
-        print(f"Erro ao abrir ou escrever em arquivos: {e}")
-
-if __name__ == "__main__":
-    main()
+        now = datetime.now()
+        date = now.date()
+        current_time = f"{now.hour:02}:{now.minute:02}:{now.second:02}"
+		
+        error_message = f"{date} {current_time} Erro: {e}. Tentando novamente em 5 segundos...\n"
+        print(error_message)
+        with open(sec_backup, "a") as file_append:
+            file_append.write(error_message)
+		
+        		
+    time.sleep(5) 
+        
